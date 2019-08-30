647055c40b14e91ed6ff1851eacfee5d092a65e5
Morgan Thompson <morgan.thompson@gov.bc.ca>
2019-08-30 16:13:01 -0700

    test old encode method

--- src/app/shared/utils/utils.ts
+++ src/app/shared/utils/utils.ts
@@ -1,21 +1,30 @@
 import { Injectable } from '@angular/core';
 
+const encode = encodeURIComponent;
+window['encodeURIComponent'] = (component: string) => {
+  return encode(component).replace(/[!'()*]/g, (c) => {
+  // Also encode !, ', (, ), and *
+    return '%' + c.charCodeAt(0).toString(16);
+  });
+};
+
 @Injectable()
 export class Utils {
   constructor() { }
 
-  public encode(component: string) {
-    let encoded = encodeURIComponent(component);
-    return encoded.replace(/[!'()*]/g, (c) => {
-    // Also encode !, ', (, ), and *
-      return '%' + c.charCodeAt(0).toString(16);
-    });
-  }
+
+  // public encode(component: string) {
+  //   let encoded = encodeURIComponent(component);
+  //   return encoded.replace(/[!'()*]/g, (c) => {
+  //   // Also encode !, ', (, ), and *
+  //     return '%' + c.charCodeAt(0).toString(16);
+  //   });
+  // }
 
   public encodeFilename(filename: string, isUrl: boolean) {
     let safeName;
     if (isUrl) {
-        return safeName = this.encode(filename).replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/\\/g, '_').replace(/\//g, '_').replace(/\%2F/g, '_');
+        return safeName = encode(filename).replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/\\/g, '_').replace(/\//g, '_').replace(/\%2F/g, '_');
     } else {
         return safeName = filename.replace(/\(/g, '%28').replace(/\)/g, '%29').replace(/\\/g, '_').replace(/\//g, '_');
     }
