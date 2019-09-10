6d4aeb157966c7dcfe8a745cb953448a303632bd
Randy P <RandyInVictoria@users.noreply.github.com>
2019-09-09 20:54:04 -0700

    EE-48 Advanced Search on Project Documents (#130)
    * EE-48 Advanced Search on Project Documents
    
    * EE-48 Clean up and fix for npm prod build

--- src/app/project/documents/documents-resolver.service.ts
+++ src/app/project/documents/documents-resolver.service.ts
@@ -2,10 +2,20 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 
+import 'rxjs/add/operator/switchMap';
+
+import * as _ from 'lodash';
+
 import { SearchService } from 'app/services/search.service';
 
 @Injectable()
 export class DocumentsResolver implements Resolve<Observable<object>> {
+  private milestones: any[] = [];
+  private authors: any[] = [];
+  private types: any[] = [];
+
+  private filterForAPI: object = {};
+
   constructor(
     private searchService: SearchService
   ) { }
@@ -16,14 +26,78 @@ export class DocumentsResolver implements Resolve<Observable<object>> {
     const pageSize = route.params.pageSize ? route.params.pageSize : 10;
     const sortBy = route.params.sortBy && route.params.sortBy !== 'null' ? route.params.sortBy : '-datePosted';
     const keywords = route.params.keywords;
-    return this.searchService.getSearchResults(
-      keywords,
-      'Document',
-      [{ 'name': 'project', 'value': projectId }],
-      currentPage,
-      pageSize,
-      sortBy,
-      { documentSource: 'PROJECT' },
-      true);
+
+    // Get the lists first
+    return this.searchService.getFullList('List')
+      .switchMap((res: any) => {
+        if (res.length > 0) {
+          res[0].searchResults.map(item => {
+            switch (item.type) {
+              case 'label':
+                this.milestones.push({ ...item });
+                break;
+              case 'author':
+                this.authors.push({ ...item });
+                break;
+              case 'doctype':
+                this.types.push({ ...item });
+                break;
+              default:
+                break;
+            }
+          });
+        }
+
+        // Validate the filter parameters being sent to the API
+        this.setFilterFromParams(route.params);
+
+        return this.searchService.getSearchResults(
+          keywords,
+          'Document',
+          [{ 'name': 'project', 'value': projectId }],
+          currentPage,
+          pageSize,
+          sortBy,
+          { documentSource: 'PROJECT' },
+          true,
+          null,
+          this.filterForAPI);
+      });
+  }
+
+  paramsToCollectionFilter(params, name, collection, identifyBy) {
+    delete this.filterForAPI[name];
+
+    if (params[name] && collection) {
+      let confirmedValues = [];
+      // look up each value in collection
+      const values = params[name].split(',');
+      values.forEach(value => {
+        const record = _.find(collection, [ identifyBy, value ]);
+        if (record) {
+          confirmedValues.push(value);
+        }
+      });
+      if (confirmedValues.length) {
+        this.filterForAPI[name] = confirmedValues.join(',');
+      }
+    }
+  }
+
+  paramsToDateFilter(params, name) {
+    delete this.filterForAPI[name];
+
+    if (params[name]) {
+      this.filterForAPI[name] = params[name];
+    }
+  }
+
+  setFilterFromParams(params) {
+    this.paramsToCollectionFilter(params, 'milestone', this.milestones, '_id');
+    this.paramsToCollectionFilter(params, 'documentAuthorType', this.authors, '_id');
+    this.paramsToCollectionFilter(params, 'type', this.types, '_id');
+
+    this.paramsToDateFilter(params, 'datePostedStart');
+    this.paramsToDateFilter(params, 'datePostedEnd');
   }
 }
