17e9ab1c0e2fe68e6fe6d46621ffaeffb450df5a
Alastair Beaumont <a.beaumont11@gmail.com>
2019-11-08 09:40:34 -0800

    Revert "Api project update (#164)"
    This reverts commit 0dd1ba6e588c451bc3fde2344efdde5422cc9e77.

--- src/app/services/project.service.ts
+++ src/app/services/project.service.ts
@@ -12,12 +12,11 @@ import { ApiService } from './api';
 import { CommentPeriodService } from './commentperiod.service';
 import { CommentPeriod } from 'app/models/commentperiod';
 import { Org } from 'app/models/organization';
-import { SearchResults, ISearchResults } from 'app/models/search';
+import { SearchResults } from 'app/models/search';
 import { flatMap } from 'rxjs/operators';
 import { of } from 'rxjs/internal/observable/of';
 import { forkJoin } from 'rxjs';
 import { SearchService } from './search.service';
-import { Utils } from 'app/shared/utils/utils';
 
 interface GetParameters {
   getresponsibleEPD?: boolean;
@@ -33,27 +32,33 @@ export class ProjectService {
   constructor(
     private api: ApiService,
     private commentPeriodService: CommentPeriodService,
-    private searchService: SearchService,
-    private utils: Utils
+    private searchService: SearchService
   ) { }
 
   // get just the projects (for fast mapping)
   getAll(pageNum: number = 0, pageSize: number = 1000000, regionFilters: object = {}, cpStatusFilters: object = {}, appStatusFilters: object = {},
     applicantFilter: string = null, clFileFilter: string = null, dispIdFilter: string = null, purposeFilter: string = null): Observable<Project[]> {
-      return this.searchService.getSearchResults('', 'Project', null, pageNum, pageSize, '', '', {}, true, '',  {})
-      .map((res: ISearchResults<Project>[]) => {
+    const regions: Array<string> = [];
+    const cpStatuses: Array<string> = [];
+    const appStatuses: Array<string> = [];
+
+    // convert array-like objects to arrays
+    Object.keys(regionFilters).forEach(key => { if (regionFilters[key]) { regions.push(key); } });
+    Object.keys(cpStatusFilters).forEach(key => { if (cpStatusFilters[key]) { cpStatuses.push(key); } });
+    Object.keys(appStatusFilters).forEach(key => { if (appStatusFilters[key]) { appStatuses.push(key); } });
+
+    return this.api.getProjects(pageNum, pageSize, regions, cpStatuses, appStatuses, applicantFilter, clFileFilter, dispIdFilter, purposeFilter)
+      .map(res => {
         if (res) {
-          const results = this.utils.extractFromSearchResults(res);
-          // let projects: Array<Project> = [];
           this.projectList = [];
-          results.forEach(project => {
+          res.forEach(project => {
             this.projectList.push(new Project(project));
           });
-          return { totalCount: res[0].data.meta[0].searchResultsTotal, data: this.projectList };
+          return this.projectList;
         }
-        return {};
+        return [];
       })
-      .catch(error => this.api.handleError(error));
+      .catch(this.api.handleError);
   }
 
   // get count of projects
@@ -90,20 +95,16 @@ export class ProjectService {
     if (this.project && this.project._id === projId && !forceReload) {
       return Observable.of(this.project);
     }
-    return this.searchService.getSearchResults('', 'Project', null, null, 1, '', '', {_id: projId}, true, '',  {})
-      .map((projects: ISearchResults<Project>[]) => {
-        let results;
-        // get upcoming comment period if there is one and convert it into a comment period object.
-        if (projects.length > 0) {
-          results = this.utils.extractFromSearchResults(projects);
-          if (results[0].commentPeriodForBanner && results[0].commentPeriodForBanner.length > 0) {
-            results[0].commentPeriodForBanner = new CommentPeriod(results[0].commentPeriodForBanner[0]);
-          } else {
-            results[0].commentPeriodForBanner = null;
-          }
+    // first get the project
+    return this.api.getProject(projId, cpStart, cpEnd)
+      .map(projects => {
+        if (projects[0].commentPeriodForBanner && projects[0].commentPeriodForBanner.length > 0) {
+          projects[0].commentPeriodForBanner = new CommentPeriod(projects[0].commentPeriodForBanner[0]);
+        } else {
+          projects[0].commentPeriodForBanner = null;
         }
         // return the first (only) project
-        return results.length > 0 ? new Project(results[0]) : null;
+        return projects.length > 0 ? new Project(projects[0]) : null;
       })
       .pipe(
         flatMap(res => {
@@ -124,7 +125,7 @@ export class ProjectService {
           );
         })
       )
-      .catch(error => this.api.handleError(error));
+      .catch(this.api.handleError);
   }
 
   getPins(proj: string, pageNum: number, pageSize: number, sortBy: any): Observable<Org> {
