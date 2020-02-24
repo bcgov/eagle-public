60c5cb3ec9c107b8761dc9f60040d86f0bb2b06b
Kit Armstrong <kitarmstrongbc@gmail.com>
2020-02-24 15:04:56 -0800

    add legistaltion

--- src/app/project/details-sidebar/details-sidebar.component.ts
+++ src/app/project/details-sidebar/details-sidebar.component.ts
@@ -1,5 +1,7 @@
-import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Component, Input, Output, EventEmitter, OnInit } from '@angular/core';
+
 import { Project } from 'app/models/project';
+import { Constants } from 'app/shared/utils/constants';
 
 @Component({
   selector: 'details-sidebar',
@@ -7,14 +9,25 @@ import { Project } from 'app/models/project';
   styleUrls: ['./details-sidebar.component.scss']
 })
 
-export class DetailsSidebarComponent {
+export class DetailsSidebarComponent implements OnInit {
   @Input() project: Project;
   @Output() onSidebarToggle: EventEmitter<any> = new EventEmitter();
 
   public sidebarOpen = true;
+  public legislationLink: string;
 
   constructor(
   ) { }
+
+  ngOnInit() {
+    if (this.project.legislation.includes('2002')) {
+      this.legislationLink = Constants.legislationLinks.ENVIRONMENTAL_ASSESSMENT_ACT_2002_LINK;
+    } else if  (this.project.legislation.includes('1996')) {
+      this.legislationLink = Constants.legislationLinks.ENVIRONMENTAL_ASSESSMENT_ACT_1996_LINK;
+    } else {
+      this.legislationLink = Constants.legislationLinks.ENVIRONMENTAL_ASSESSMENT_ACT_2018_LINK;
+    }
+  }
 
   toggleSidebar() {
     this.sidebarOpen = !this.sidebarOpen;
