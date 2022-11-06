66515c0817a66e4792688dcb0e6dd9e68f0675ad
dinesh <dinesh.pb@aot-technologies.com>
2022-11-06 10:48:34 +0530

    revert(bd98ef18e0bdfa89531f49331945f2db5c19a6ac-0c09657a9eb6aa22fd1eca7d5bdef26909138cb2)

--- src/app/projects/project-list/project-list-table-rows/project-list-table-rows.component.ts
+++ src/app/projects/project-list/project-list-table-rows/project-list-table-rows.component.ts
@@ -1,9 +1,7 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 
 import { Router } from '@angular/router';
-import { ApiService } from 'app/services/api';
-import { FavouriteService } from 'app/services/favourite.service';
-import { ITableMessage, TableRowComponent } from 'app/shared/components/table-template-2/table-row-component';
+import { TableRowComponent } from 'app/shared/components/table-template-2/table-row-component';
 
 @Component({
     selector: 'app-project-list-table-rows',
@@ -12,35 +10,13 @@ import { ITableMessage, TableRowComponent } from 'app/shared/components/table-te
 })
 
 export class ProjectListTableRowsComponent extends TableRowComponent {
-    @Output() updateFavourites: EventEmitter<ITableMessage> = new EventEmitter<ITableMessage>();
-
     constructor(
-        private router: Router,
-        public apiService: ApiService,
-        public favouriteService: FavouriteService,
+        private router: Router
     ) {
         super();
     }
 
     goToProject(project) {
         this.router.navigate([`p/${project._id}/project-details`]);
-    }
-
-    public addToFavourite(item, type: string = 'Project') {
-      this.apiService.addFavourite(item, type)
-        .then(() => {
-          this.updateFavourites.emit({data: {type}, label: 'Update Favourite'});
-        }).catch((err) => {
-          console.log('error adding favourite', err)
-        });
-    }
-
-    public removeFavourite(item) {
-      this.apiService.removeFavourite(item)
-        .then(() => {
-          this.updateFavourites.emit({data: {type: 'Project'}, label: 'Update Favourite'});
-        }).catch((err) => {
-          console.log('error removing favourite', err)
-        });
     }
 }
