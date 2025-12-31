20b71d9983592d688a53f53380c0a9a48d1d0d1e
Daniel Truong <daniel@digitalspace.ca>
2025-12-31 01:18:37 +0000

    Fix documents tab and implement responsive mobile UI
    - Fix documents tab display by using toObservable to avoid circular dependencies
    - Implement responsive breakpoint at 1073px for mobile layout
    - Stack sidebar on top of content for screens ≤1073px
    - Set default sort to date posted (descending) instead of featured
    - Fix pagination by using local variables to avoid signal dependencies
    - Clean up code: remove duplicate declarations, simplify filter building
    - Update tab-nav and details-sidebar CSS for consistent mobile behavior

--- src/app/project/documents/documents-tab.component.ts
+++ src/app/project/documents/documents-tab.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, OnDestroy, inject, signal, effect, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, OnDestroy, inject, signal, ChangeDetectionStrategy } from '@angular/core';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { takeWhile } from 'rxjs/operators';
+import { toObservable } from '@angular/core/rxjs-interop';
 import { SearchResults } from '../../models/search';
 import { SearchParamObject } from '../../services/search.service';
 import { DocumentTableRowsComponent } from './project-document-table-rows/project-document-table-rows.component';
@@ -13,6 +14,7 @@ import { ConfigService } from '../../services/config.service';
 import { TableService } from '../../services/table.service';
 import { TableTemplateComponent } from '../../shared/components/table-template/table-template.component';
 import { SearchFilterTemplateComponent } from '../../shared/components/search-filter-template/search-filter-template.component';
+import { LoggingService } from '../../services/logging.service';
 
 @Component({
   selector: 'app-documents',
@@ -28,9 +30,9 @@ export class DocumentsTabComponent implements OnInit, OnDestroy {
   private readonly tableTemplateUtils = inject(TableTemplate);
   private readonly tableService = inject(TableService);
   private readonly configService = inject(ConfigService);
+  private readonly logger = inject(LoggingService);
 
   private readonly tableId = 'documentsTab';
-  private readonly tableSignal = this.tableService.getTableSignal(this.tableId);
   private alive = true;
   private lists: any[] = [];
   private initialLoad = true;
@@ -46,7 +48,10 @@ export class DocumentsTabComponent implements OnInit, OnDestroy {
   public queryParams: Params = {};
   public readonly showAdvancedFilters = signal(false);
   public readonly filters = signal<FilterObject[]>([]);
-  public readonly tableData = signal<TableObject>(new TableObject({ component: DocumentTableRowsComponent }));
+  public readonly tableData = signal<TableObject>(new TableObject({ 
+    component: DocumentTableRowsComponent,
+    sortBy: '-datePosted' // Default sort: date posted (descending)
+  }));
 
   public readonly tableColumns: IColumnObject[] = [
     {
@@ -82,30 +87,39 @@ export class DocumentsTabComponent implements OnInit, OnDestroy {
   ];
 
   private readonly legislationFilterGroup = { name: 'legislation', labelPrefix: '', labelPostfix: ' Act Terms' };
+  
+  private readonly tableSignal$ = toObservable(this.tableService.getTableSignal(this.tableId));
 
-  constructor() {
-    // Watch for table data changes from service
-    effect(() => {
-      const searchResults = this.tableSignal();
-      
-      if (searchResults && searchResults.data && searchResults.data !== 0) {
-        const currentTableData = this.tableData();
-        currentTableData.totalListItems = searchResults.totalSearchCount;
-        currentTableData.items = searchResults.data.map((record: any) => {
-          record['showFeatured'] = true;
-          return { rowData: record };
-        });
-        currentTableData.columns = this.tableColumns;
-        currentTableData.options.showAllPicker = true;
-
-        this.tableData.set(currentTableData);
-      }
-    });
-  }
+  constructor() {}
 
   ngOnInit() {
     // Get project ID from parent route
     this.projId = this.route.parent?.snapshot.params['projId'] || '';
+    this.logger.debug(`Documents tab projId: ${this.projId}`, 'DocumentsTabComponent');
+
+    // Watch for table data changes from service
+    this.tableSignal$.pipe(takeWhile(() => this.alive)).subscribe((searchResults: SearchResults) => {
+      if (searchResults && searchResults.data !== undefined) {
+        const currentTableData = this.tableData();
+        // Create new TableObject to ensure change detection with OnPush
+        const updatedTableData = new TableObject({
+          component: DocumentTableRowsComponent,
+          currentPage: currentTableData.currentPage,
+          pageSize: currentTableData.pageSize,
+          sortBy: currentTableData.sortBy
+        });
+        
+        updatedTableData.totalListItems = searchResults.totalSearchCount || 0;
+        updatedTableData.items = (searchResults.data || []).map((record: any) => {
+          record['showFeatured'] = true;
+          return { rowData: record };
+        });
+        updatedTableData.columns = this.tableColumns;
+        updatedTableData.options.showAllPicker = true;
+        
+        this.tableData.set(updatedTableData);
+      }
+    });
 
     this.configService.lists.pipe(takeWhile(() => this.alive)).subscribe((list) => {
       this.lists = list;
@@ -125,7 +139,14 @@ export class DocumentsTabComponent implements OnInit, OnDestroy {
 
     this.route.queryParamMap.pipe(takeWhile(() => this.alive)).subscribe(data => {
       this.queryParams = { ...(data as any)['params'] };
-      const updatedTableData = this.tableTemplateUtils.updateTableObjectWithUrlParams((data as any)['params'], this.tableData());
+      const currentTableData = this.tableData();
+      const updatedTableData = this.tableTemplateUtils.updateTableObjectWithUrlParams((data as any)['params'], currentTableData);
+      
+      // If no sortBy in URL, use date as default for documents tab
+      if (!(data as any)['params'].sortBy) {
+        updatedTableData.sortBy = '-datePosted';
+      }
+      
       this.tableData.set(updatedTableData);
 
       if (
@@ -143,30 +164,30 @@ export class DocumentsTabComponent implements OnInit, OnDestroy {
 
       // Build filters object from query params
       const filters: Record<string, string> = {};
-      this.filtersList.forEach(filterKey => {
-        if (this.queryParams[filterKey]) {
-          filters[filterKey] = this.queryParams[filterKey];
-        }
-      });
-      this.dateFiltersList.forEach(filterKey => {
+      [...this.filtersList, ...this.dateFiltersList].forEach(filterKey => {
         if (this.queryParams[filterKey]) {
           filters[filterKey] = this.queryParams[filterKey];
         }
       });
       
-      // Fetch data with current params
-      const currentTableData = this.tableData();
+      // Fetch data with current params (use local variable, not signal)
+      this.logger.debug(`Fetching documents with projId: ${this.projId}`, 'DocumentsTabComponent', {
+        currentPage: updatedTableData.currentPage,
+        pageSize: updatedTableData.pageSize,
+        sortBy: updatedTableData.sortBy,
+        filters
+      });
       this.tableService.fetchData(new SearchParamObject(
         this.tableId,
         this.queryParams['keywords'] || '',
         'Document',
         [],
-        currentTableData.currentPage,
-        currentTableData.pageSize,
-        currentTableData.sortBy,
+        updatedTableData.currentPage,
+        updatedTableData.pageSize,
+        updatedTableData.sortBy,
         { project: this.projId },
         true,
-        currentTableData.sortBy.includes('displayName') ? '' : '+displayName',
+        updatedTableData.sortBy.includes('displayName') ? '' : '+displayName',
         filters
       ));
     });
@@ -259,7 +280,8 @@ export class DocumentsTabComponent implements OnInit, OnDestroy {
       }
     } else {
       params['keywords'] = null;
-      params['sortBy'] = Constants.tableDefaults.DEFAULT_SORT_BY;
+      // For documents tab, default to date posted (descending)
+      params['sortBy'] = '-datePosted';
     }
 
     params['currentPage'] = 1;
